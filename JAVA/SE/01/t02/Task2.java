f363e85848f5a8bc51519ef084d5f07abfd84b48
Konstantin <kostkol87@gmail.com>
2015-11-25 19:15:30 +0400

    second task added

---
+++ JAVA/SE/01/t02/Task2.java
@@ -0,0 +1,24 @@
+package t02;
+
+public class Task2 {
+    static final double EPS = 0.000001;
+
+    private strictfp void findMin(){
+        double a;
+        double tmp = 0;
+        for (int i = 1;; i++) {
+            a = 1/(Math.pow((i + 1), 2));
+            if(a > EPS){
+                System.out.println(i + " : " + a);
+            }else {
+                System.out.println("minimum at index " + (i - 1) + " with value " + tmp);
+                break;
+            }
+            tmp = a;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Task2().findMin();
+    }
+}
