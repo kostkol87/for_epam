4456197bb9a351f025502bad7f15bd1358d85d65
Konstantin <kostkol87@gmail.com>
2015-12-02 20:50:43 +0400

    second hw first task added

---
+++ src/main/java/HW02/t01/Pen.java
@@ -0,0 +1,19 @@
+package HW02.t01;
+
+public class Pen {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
