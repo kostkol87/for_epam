e2ab819d0f9967a3f57236d84b6836f4d1398651
kostkol87 <kostkol87@gmail.com>
2015-12-03 19:58:58 +0200

    added implementation of equals and hashcode (1st task)

--- src/main/java/HW02/t01/Pen.java
+++ src/main/java/HW02/t01/Pen.java
@@ -1,15 +1,35 @@
 package HW02.t01;
 
+import java.awt.*;
+
 public class Pen {
+    private String label;
+    private Color color;
+
+    Pen(Color color, String label){
+        this.color = color;
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return label.length()*color.hashCode();
     }
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+
+        if(obj == null || !(obj instanceof Pen)) return false;
+        Pen o = (Pen) obj;
+        return (getLabel().equals(o.getLabel()) && getColor().equals(o.getColor()));
     }
 
     @Override
