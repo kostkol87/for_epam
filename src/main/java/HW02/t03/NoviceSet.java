3c28c553b0239661cfd4615aa618c43ec0585759
Konstantin <kostkol87@gmail.com>
2015-12-09 19:03:50 +0400

    some fixes in 3rd task

--- src/main/java/HW02/t03/NoviceSet.java
+++ src/main/java/HW02/t03/NoviceSet.java
@@ -3,7 +3,7 @@ package HW02.t03;
 import java.util.ArrayList;
 
 public class NoviceSet {
-    public ArrayList<Stationery> getNoviceSet(){
+    public ArrayList<Stationery> getNoviceSet() {
         ArrayList<Stationery> noviceSet = new ArrayList<Stationery>();
         noviceSet.add(new Pen("Parker", 100.0));
         noviceSet.add(new Pen("Parker", 59.9));
@@ -14,11 +14,17 @@ public class NoviceSet {
         noviceSet.add(new Rubber("LYRA", 5));
         noviceSet.add(new Rubber("LYRA", 5));
         noviceSet.add(new Corrector("BiC", 100));
-        noviceSet.add(new Marker("MOLOTOW",15));
+        noviceSet.add(new Marker("MOLOTOW", 15));
         noviceSet.add(new Paper("Bereg", 300));
         noviceSet.add(new Sticker("DeliMemoPaper", 35));
         noviceSet.add(new Stapler("Swingline", 231.5));
         noviceSet.add(new Clip("SteelClip", 10));
         return noviceSet;
+    }
+
+    public void printNoviceSet(ArrayList<Stationery> noviceSet) {
+        for (Stationery element : noviceSet) {
+            System.out.println(element);
+        }
     }
 }
