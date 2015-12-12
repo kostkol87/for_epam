d57f913046dce6f4551caaec3e034415e6176110
kostkol87 <kostkol87@gmail.com>
2015-12-13 01:11:44 +0200

    second task of 3rd HW complete

--- src/main/java/HW03/t02/LocaleAndRB.java
+++ src/main/java/HW03/t02/LocaleAndRB.java
@@ -1,31 +1,55 @@
 package HW03.t02;
 
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
 import java.util.Enumeration;
 import java.util.Locale;
-import java.util.NoSuchElementException;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class LocaleAndRB {
-    ResourceBundle bundle;
 
-    public LocaleAndRB(Locale locale){
-        bundle = ResourceBundle.getBundle("resource", locale);
+    private ResourceBundle bundle_q;
+    private ResourceBundle bundle_a;
+    private Scanner scanner = new Scanner(System.in);
 
+    public void setBundleLocale(Locale locale) {
+        bundle_q = ResourceBundle.getBundle("questions", locale);
+        bundle_a = ResourceBundle.getBundle("answers", locale);
+
+        Enumeration<String> keys = bundle_q.getKeys();
+
+        while (keys.hasMoreElements()) {
+            String s = keys.nextElement();
+            System.out.println(s.substring(1) + ". " + bundle_q.getString(s));
+        }
+    }
+
+    public void getBundleQuestions(int q) {
+        System.out.println(bundle_a.getString("q" + q));
+    }
+
+    public void quiz() {
+        int i;
+        System.out.println("Select language/Выберите язык:");
+        System.out.println("1. English");
+        System.out.println("2. Русский");
+        System.out.print(">>> ");
+        i = scanner.nextInt();
+        switch (i) {
+            case 1: {
+                setBundleLocale(new Locale("en", "US"));
+                break;
+            }
+            case 2: {
+                setBundleLocale(new Locale("ru", "RU"));
+                break;
+            }
+        }
+        System.out.print(">>> ");
+        getBundleQuestions(scanner.nextInt());
     }
 
     public static void main(String[] args) {
-        LocaleAndRB localeAndRB = new LocaleAndRB(new Locale("en", "US"));
-        Enumeration<String> keys = localeAndRB.bundle.getKeys();
-        String s;
-        try {
-            while ((s = keys.nextElement()) != null){
-                System.out.println(s);
-            }
-        }catch (NoSuchElementException e){
-
-        }
+        LocaleAndRB localeAndRB = new LocaleAndRB();
+        localeAndRB.quiz();
     }
 }
