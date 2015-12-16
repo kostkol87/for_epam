a96a6c1a0b680aa9824728aafbde2e27df744baf
kostkol87 <kostkol87@gmail.com>
2015-12-17 00:15:47 +0200

    added part of 3rd tas 3rd module

--- src/main/java/HW03/t03/RegExp.java
+++ src/main/java/HW03/t03/RegExp.java
@@ -1,26 +1,64 @@
 package HW03.t03;
 
 import javax.swing.text.BadLocationException;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExp {
     private final String filePath = System.getProperty("user.dir") + "/src/main/resources/Java.SE.03.Information handling_task_attachment.html";
-    String html;
-    public void collectFromHTML() throws IOException, BadLocationException {
-        Pattern pattern = Pattern.compile("");
+    private final String output = System.getProperty("user.dir") + "/src/main/resources/output.html";
+    StringBuilder html = new StringBuilder();
+
+
+    public String collectFromHTML() throws IOException, BadLocationException {
+
         BufferedReader in = new BufferedReader(new FileReader(filePath));
         String tmp;
-        while ((tmp = in.readLine()) != null){
-            html += tmp;
+        while ((tmp = in.readLine()) != null) {
+            html.append(tmp);
         }
         System.out.println(html);
+        return html.toString();
+    }
+
+    public boolean isConseq(String html) {
+        Pattern pattern = Pattern.compile("(\\([р|Р]ис.|[р|Р]исунке)\\s(\\d+)\\)?");
+        Matcher matcher = pattern.matcher(html);
+        int tmp = 0;
+        while (matcher.find()) {
+            int i = Integer.valueOf(matcher.group(2));
+            if (tmp < i) {
+                tmp = i;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String markSentences(String html) throws IOException, BadLocationException {
+        Pattern sentence = Pattern.compile("(?![^\\.]\\s+|\\s+)(?![^\\.]\\s+[\\(\\\"`'])([\\\"\\`\\']?[А-ЯA-Z][^\\.\\!\\?]*((\\([р|Р]ис.|[р|Р]исунке)\\s(\\d+)\\)?)+(.)*?)(?=[\\.\\!\\?](\\s|\\Z))");
+        Matcher matcherS = sentence.matcher(html);
+        ArrayList<String> sentences = new ArrayList<>();
+        while (matcherS.find()) {
+//            System.out.println(matcherS.group());
+            sentences.add(matcherS.group());
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+        System.out.println("in " + html);
+        for (String str : sentences) {
+            html = html.replace(str, "<b>" + str + "</b>");
+        }
+        writer.write(html);
+        writer.close();
+        return sentences.size() + " sentences was marked";
     }
 
     public static void main(String[] args) throws IOException, BadLocationException {
         RegExp regExp = new RegExp();
-        regExp.collectFromHTML();
+        System.out.println(regExp.isConseq(regExp.collectFromHTML()));
+        regExp.markSentences(regExp.collectFromHTML());
     }
 }
