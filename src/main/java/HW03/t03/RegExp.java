31a172b1a85f2b483229ac45ca4074649ffb9b01
kostkol87 <kostkol87@gmail.com>
2015-12-17 21:44:43 +0200

    complete 3rd task of 3rd module

--- src/main/java/HW03/t03/RegExp.java
+++ src/main/java/HW03/t03/RegExp.java
@@ -7,19 +7,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExp {
-    private final String filePath = System.getProperty("user.dir") + "/src/main/resources/Java.SE.03.Information handling_task_attachment.html";
-    private final String output = System.getProperty("user.dir") + "/src/main/resources/output.html";
-    StringBuilder html = new StringBuilder();
 
+    final String FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/Java.SE.03.Information handling_task_attachment.html";
+    final String OUTPUT = System.getProperty("user.dir") + "/src/main/resources/output.html";
 
-    public String collectFromHTML() throws IOException, BadLocationException {
-
-        BufferedReader in = new BufferedReader(new FileReader(filePath));
+    public String collectFromHTML(String file) throws IOException, BadLocationException {
+        StringBuilder html = new StringBuilder();
+        BufferedReader in = new BufferedReader(new FileReader(file));
         String tmp;
         while ((tmp = in.readLine()) != null) {
-            html.append(tmp);
+            html.append(tmp.trim());
         }
-        System.out.println(html);
         return html.toString();
     }
 
@@ -38,27 +36,19 @@ public class RegExp {
         return true;
     }
 
-    public String markSentences(String html) throws IOException, BadLocationException {
-        Pattern sentence = Pattern.compile("(?![^\\.]\\s+|\\s+)(?![^\\.]\\s+[\\(\\\"`'])([\\\"\\`\\']?[А-ЯA-Z][^\\.\\!\\?]*((\\([р|Р]ис.|[р|Р]исунке)\\s(\\d+)\\)?)+(.)*?)(?=[\\.\\!\\?](\\s|\\Z))");
+    public String markSentences(String html, String output) throws IOException, BadLocationException {
+        Pattern sentence = Pattern.compile("(?![^\\.]\\s+|\\s+<div>|\\s+<p>)(?![^\\.]\\s+[\\(\\\"`'])([\\\"\\`\\']?[А-ЯA-Z][^\\.\\!\\?]*((\\([р|Р]ис\\.|[р|Р]исунке)\\s(\\d+)\\)?)+(.)*?)(?=[\\.\\!\\?](\\s|\\Z|</div>|</p>))");
         Matcher matcherS = sentence.matcher(html);
         ArrayList<String> sentences = new ArrayList<>();
         while (matcherS.find()) {
-//            System.out.println(matcherS.group());
             sentences.add(matcherS.group());
         }
         BufferedWriter writer = new BufferedWriter(new FileWriter(output));
-        System.out.println("in " + html);
         for (String str : sentences) {
-            html = html.replace(str, "<b>" + str + "</b>");
+            html = html.replace(str, "<font size=\"5\" color=\"red\" face=\"Arial\">" + str + "</font>");
         }
         writer.write(html);
         writer.close();
         return sentences.size() + " sentences was marked";
-    }
-
-    public static void main(String[] args) throws IOException, BadLocationException {
-        RegExp regExp = new RegExp();
-        System.out.println(regExp.isConseq(regExp.collectFromHTML()));
-        regExp.markSentences(regExp.collectFromHTML());
     }
 }
---
+++ src/test/java/HW03/t03/RegExpTest.java
@@ -0,0 +1,38 @@
+package HW03.t03;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class RegExpTest {
+
+    @Test
+    public void testCollectFromHTML() throws Exception {
+        RegExp regExp = new RegExp();
+        String s = regExp.collectFromHTML(regExp.FILE_PATH);
+        Assert.assertTrue(s.contains("элементарные коллапсары (описание элементарных и индуцированных коллапсаров дано в книге)."));
+    }
+
+    @Test
+    public void testIsConseq() throws Exception {
+        RegExp regExp = new RegExp();
+        boolean conseq = regExp.isConseq(regExp.collectFromHTML(regExp.FILE_PATH));
+        Assert.assertFalse(conseq);
+    }
+
+    @Test
+    public void testMarkSentences() throws Exception {
+        RegExp regExp = new RegExp();
+        regExp.markSentences(regExp.collectFromHTML(regExp.FILE_PATH), regExp.OUTPUT);
+
+        BufferedReader in = new BufferedReader(new FileReader(regExp.OUTPUT));
+        StringBuilder html = new StringBuilder();
+        String tmp;
+        while ((tmp = in.readLine()) != null) {
+            html.append(tmp.trim());
+        }
+        Assert.assertTrue(html.toString().contains("<font size=\"5\" color=\"red\" face=\"Arial\">"));
+    }
+}
